516656a8803bc60f4b9aab2b19b55bf5e6f1d8dc
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-05-23 21:03:46 -0300

    feature: start scan and stop scan methods

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -16,6 +16,7 @@ class ScanScreen extends StatefulWidget {
 class _ScanScreenState extends State<ScanScreen> {
 
   bool _isBluetoothActive = false;
+  bool _isScanning = false;
   List<ScanResult> _devices = [];
   String _status = '';
   BluetoothDevice? _device;
@@ -33,6 +34,33 @@ class _ScanScreenState extends State<ScanScreen> {
      super.dispose();
   }
 
+  Future<void> _startScan() async {
+    try {
+    setState(() {
+    _isScanning = true;
+    _status = 'Escaneando dispositivos próximos...';  
+    });
+    await FlutterBluePlus.startScan(timeout: Duration(seconds: 5));
+    setState(() {
+      _isScanning = false;
+      _status = 'Scan completo!';
+    });
+    } catch (error) {
+      setState(() => _status = 'Erro ao iniciar scan, verifique se concedeu permissão ao app para usar o Bluetooth.');
+    }
+  }
+
+  Future<void> _stopScan () async {
+    try {
+    await FlutterBluePlus.stopScan();
+    setState(() {
+    _isScanning = false;
+    _status = 'Scan interrompido!';  
+    });
+    } catch (error) {
+      setState(() => _status = 'Não foi possível parar o scan, espere alguns segundos e tente novamente.');
+    }
+  }
 
   Future<void> _checkBluetooth() async {
     var isSupported = await FlutterBluePlus.isSupported;
