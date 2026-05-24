f4c13b1c46720570ec8336ac8ad75d8a0c3edd7c
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-05-23 22:45:48 -0300

    feature: test interface for route calling

---
+++ lib/screens/control_screen.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class ControlScreen extends StatefulWidget {
+
+  final BluetoothDevice device;
+  
+  ControlScreen({super.key, required this.device});
+
+  State<ControlScreen> createState() => _ControlScreenState();
+}
+
+class _ControlScreenState extends State<ControlScreen> {
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.device.platformName)),
+      body: const Center(child: Text('Tela de Controle — em breve')),
+    );
+  }
+}
